f2e01c2a2bba99d9c40a72c39b9839e876c3b3fa
vidan997 <milojevic27116@fon.bg.ac.rs>
2018-03-10 14:57:48 +0100

    Impelentirane metode za rad sa valutama.

--- Menjacnica/src/menjacnica/interfejs/menjacnicainterfejs.java
+++ Menjacnica/src/menjacnica/interfejs/menjacnicainterfejs.java
@@ -3,7 +3,7 @@ package menjacnica.interfejs;
 public interface menjacnicainterfejs {
 	
 	public void dodavanjeValute(String datum, String naziv, int srednjiKurs, int prodajniKurs, int kupovniKurs);
-	public void brisanjeValute(String datum);
-	public String vracanjeValute(String datum);
+	public void brisanjeValute(String datum, String naziv);
+	public String vracanjeValute(String datum, String naziv);
 	
 }
