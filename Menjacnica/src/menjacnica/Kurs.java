d3d2ed82cbf968e62bdba5d7bf055df5c256fcc8
vidan997 <milojevic27116@fon.bg.ac.rs>
2018-03-10 13:59:05 +0100

    Napravljene klase kurs i valuta.

---
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -0,0 +1,29 @@
+package menjacnica;
+
+public class Kurs extends Valuta {
+	
+	private double prodajniKurs;
+	private double srednjiKurs;
+	private double kupovniKurs;
+	
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	
+	
+}
---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,31 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+	
+	private String naziv;
+	private String skraceniNaziv;
+	private GregorianCalendar datum;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	
+	
+}
