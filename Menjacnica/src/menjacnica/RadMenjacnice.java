9d52eb5154fa19b1bbc87c0b85b71ea4ef8457e5
vidan997 <milojevic27116@fon.bg.ac.rs>
2018-03-10 14:31:28 +0100

    Napravljena nova klasa RadMenjacnice sa praznim metodama.

---
+++ Menjacnica/src/menjacnica/RadMenjacnice.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import menjacnica.interfejs.menjacnicainterfejs;
+
+public class RadMenjacnice implements menjacnicainterfejs {
+
+	@Override
+	public void dodavanjeValute(String datum, String naziv, int srednjiKurs, int prodajniKurs, int kupovniKurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brisanjeValute(String datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String vracanjeValute(String datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
